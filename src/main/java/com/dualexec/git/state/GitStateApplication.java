61da1c1c680c7d2752c842a6fa7b4350490a83ea
Alexej Sailer <alexejsailer@gmail.com>
2017-10-21 22:10:46 +0200

    add basic authentication using spring security

--- src/main/java/com/dualexec/git/state/GitStateApplication.java
+++ src/main/java/com/dualexec/git/state/GitStateApplication.java
@@ -2,8 +2,10 @@ package com.dualexec.git.state;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.dualexec.git.state" })
 public class GitStateApplication {
 
 	public static void main(String[] args) {
