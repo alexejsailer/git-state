bae753d127cdbfc088c50e4a8acd47f79f157bae
Alexej Sailer <alexejsailer@gmail.com>
2017-10-21 21:58:37 +0200

    initial project import

---
+++ src/main/java/com/dualexec/git/state/GitStateApplication.java
@@ -0,0 +1,12 @@
+package com.dualexec.git.state;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitStateApplication.class, args);
+	}
+}
